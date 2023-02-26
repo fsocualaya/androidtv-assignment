d711355c151bd731ec51560f87d3c29069ff35ac
Fernando Socualaya <fernando.socualaya@dysopsis.com>
2023-02-26 19:43:15 -0300

    feat: Implements main SearchView widget

---
+++ app/src/main/java/com/fsocualaya/androidtv_assignment/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.fsocualaya.androidtv_assignment
+
+import android.os.Bundle
+import android.widget.SearchView
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+
+class MainActivity : FragmentActivity() {
+    private var searchView : SearchView? = null
+    private var queryTV: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        searchView = findViewById(R.id.search_view)
+        queryTV = findViewById(R.id.query_tv)
+
+        handleSearchView()
+    }
+
+    private fun handleSearchView(){
+        searchView!!.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                queryTV!!.text = query
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
+
+
+    }
+}
